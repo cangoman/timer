7bfabec58d1e79418bad845fe891e9f0a73187c7
Camilo Gomez <camilogomezmusic@gmail.com>
2020-07-28 23:07:41 +0000

    completed the interactive timer activity

---
+++ timer2.js
@@ -0,0 +1,25 @@
+const readline = require('readline');
+readline.emitKeypressEvents(process.stdin);
+process.stdin.setRawMode(true);
+
+const beep = () => {
+  process.stdout.write('\x07');
+};
+
+process.stdout.write('Please enter an option:\nb: beep\n1-9 set a timer in seconds\nctrl=c to exit:\n');
+
+process.stdin.on('keypress', (str, key) => {
+  let timeout;
+  if (key.ctrl && key.name === 'c') {
+    process.stdout.write('Thanks for using me, ciao!\n');
+    process.exit();
+  }
+  else {
+    if (key.name === 'b') beep();
+    if (key.name > 0 && key.name < 10) {
+      timeout = Number(key.name) * 1000;
+      process.stdout.write(`setting timer for ${key.name} seconds...\n`)
+      setTimeout( beep, timeout);
+    }
+  }
+});
